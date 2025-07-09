3cc348f5b81a40b51c3f37dc3acef363b5ce8dbf
nanitKh <nanit.khanna@artesian.io>
2025-07-09 19:21:25 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControllersAndActions.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();  
+        }
+        public string Details()
+        {
+            return "Welcome!";
+        }
+        public string detailsWithId(int id)
+        {
+            return $"Welcome! Your ID is {id}";
+        }
+
+    }
+}
